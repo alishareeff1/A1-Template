634aab816bd4528a5559cc73e14f8817a04f61fb
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-24 10:42:43 -0500

    created Maze class and added variables, constructor, and makeArray method

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -0,0 +1,20 @@
+package ca.mcmaster.se2aa4.mazerunner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Maze {
+    private char[][] maze;
+    private String MazeFilePath;
+
+    public Maze(String MazeFilePath){
+        this.MazeFilePath = MazeFilePath;
+    }
+
+    public char[][]makeArray(){
+        
+    }
+}
