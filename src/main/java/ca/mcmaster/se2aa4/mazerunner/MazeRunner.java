205a8feb64c22bf31f46c9e89bc54782354063b0
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-24 11:03:26 -0500

    created MazeRunner class and added variables and methods

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.mazerunner;
+import java.util.ArrayList;
+
+public class MazeRunner {
+    private char[][] maze;
+    public MazeRunner(char[][] maze){
+        this.maze = maze;
+    }
+
+    public ArrayList<String> GenerateSequence(){
+   
+    }
+}
