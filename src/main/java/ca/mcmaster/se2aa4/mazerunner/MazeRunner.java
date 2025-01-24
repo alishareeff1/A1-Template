658c045a3d826a3ba9f1b6e856631a412f413ace
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-24 11:06:22 -0500

    implemented finding entrance of maze and generating sequence from maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
@@ -8,6 +8,20 @@ public class MazeRunner {
     }
 
     public ArrayList<String> GenerateSequence(){
-   
+        ArrayList<String> sequence = new ArrayList<>();
+        int entrance = 0;
+        int rows = maze.length;
+        int cols = maze[0].length;
+        for(int i = 0; i < rows; i++){
+            if(maze[i][1] == 'P'){
+                entrance = i;
+            }
+        }
+        for(int j = 0; j < cols; j++){
+            if(maze[entrance][j] == 'P'){
+                sequence.add("F");
+            }
+        }
+        return sequence;
     }
 }
