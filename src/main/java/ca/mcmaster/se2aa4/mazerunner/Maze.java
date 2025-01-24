1487ebd1e43b5ff874562d19e7d7c3f0d1967cbf
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-24 10:52:11 -0500

    read text file and put each line in a 2d array with spaces replaced with a 'P'

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -15,6 +15,44 @@ public class Maze {
     }
 
     public char[][]makeArray(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(MazeFilePath));
+            List<char[]> mazeList = new ArrayList<>();
+            String line;
+            line = reader.readLine();
+            int width = line.length();
+            line = line.replace(' ', 'P');
+            mazeList.add(line.toCharArray());
+            
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    line = "P".repeat(width); 
+                } else {
+                    line = line.replace(' ', 'P');
+                }
+                System.out.println("Read line: " + line);
+                char[] lineArray = new char[line.length()];
+                for(int i = 0; i < lineArray.length; i++){
+                    if(line.charAt(i) == ' '){
+                        lineArray[i] = 'P';
+                    }else{
+                        lineArray[i] = line.charAt(i);
+                    }
+                }
+                mazeList.add(lineArray);
+                
+            }
+            reader.close();
+
+            char[][] maze = new char[mazeList.size()][];
+            mazeList.toArray(maze);
+
+            return maze;
+            
+        }  catch (IOException e) {
+                System.out.println("Error reading the file: " + e.getMessage());
+                return null;
+        }
         
     }
 }
