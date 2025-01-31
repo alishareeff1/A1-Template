63095e503a1e19663b64c61ee929837c29b40311
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-31 16:30:30 -0500

    created constructor for new class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Shortest_MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Shortest_MazeRunner.java
@@ -1,6 +1,14 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Shortest_MazeRunner extends MazeRunner {
+    private char[][] maze;
+    private int entrance;
+    private int exit_row;
+    private int exit_col;
+    public Shortest_MazeRunner(char[][] maze){
+        super(maze);
+        this.maze = maze;
+    }
     
 
 }
