109c0ab6f1c3d93d73559be89b1bea07523180b2
Ali Shareeff <shareefa@mcmaster.ca>
2025-01-31 16:31:52 -0500

    created method that finds entrance

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Shortest_MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Shortest_MazeRunner.java
@@ -9,6 +9,24 @@ public class Shortest_MazeRunner extends MazeRunner {
         super(maze);
         this.maze = maze;
     }
-    
+    public void find_entrance(){
+        System.out.println("starting find entrance method");
+        entrance = -1;
+        int entrance_count = 0;
+        int rows = maze.length;
+        for(int i = 0; i < rows; i++){
+            if(maze[i][0] == 'P'){
+                entrance = i;
+                entrance_count++;
+            }
+        }
+        if(entrance == -1){
+            throw new IllegalStateException("No entrance found in first column");
+        }
+        if(entrance_count > 1){
+            throw new IllegalStateException("There can only be one entrance");
+        }
+        System.out.println(entrance);
+    }
 
 }
