ccfe7c5f01be4905509a86c80830bd2b102287f8
Ali Shareeff <shareefa@mcmaster.ca>
2025-02-02 12:24:32 -0500

    updated main to incorportate factorized_path class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -14,7 +14,7 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        logger.info("** Starting Maze Runner");
+        // logger.info("** Starting Maze Runner");
 
         Options options = new Options();
         options.addOption("i", "input", true, "Path to the maze file");
@@ -30,37 +30,31 @@ public class Main {
             }
 
             String mazeFilePath = cmd.getOptionValue("i");
-            logger.info("Reading the maze from file: " + mazeFilePath);
+            // logger.info("Reading the maze from file: " + mazeFilePath);
 
             Maze maze = new Maze(mazeFilePath);
             char[][] mazeArray = maze.makeArray();
-            if (mazeArray != null) {
-                for (int i = 0; i < mazeArray.length; i++) {
-                    for (int j = 0; j < mazeArray[i].length; j++) {
-                        System.out.print(mazeArray[i][j]);
-                    }
-                    System.out.println();
-                }
-            }
-
-            Shortest_MazeRunner sequence = new Shortest_MazeRunner(mazeArray);
+            Right_Hand_MazeRunner sequence = new Right_Hand_MazeRunner(mazeArray);
             sequence.find_entrance();
             sequence.find_exit();
 
-            logger.info("**** Computing path");
+            // logger.info("**** Computing path");
             String computedSequence = sequence.Add_Sequence();
-            logger.info("Sequence of moves is: " + computedSequence);
+            Factorized_Path factorized = new Factorized_Path(computedSequence);
+            String seq = factorized.Compress_Moves();
+
+            System.out.println("Sequence of moves is: " + seq);
 
             if (cmd.hasOption("p")) {
                 String userSequence = cmd.getOptionValue("p");
-                if (computedSequence.equals(userSequence)) {
+                if (seq.equals(userSequence)) {
                     logger.info("Correct path entered!");
                 } else {
                     logger.info("Incorrect path entered.");
                 }
             }
 
-            logger.info("** End of MazeRunner");
+            // logger.info("** End of MazeRunner");
         } catch (Exception e) {
             logger.error("/!\\ An error has occurred /!\\", e);
         }
